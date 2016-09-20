d320032212f6480793de301d532d0506910b22cb
Sergii Illichevskyi <Sergii_Illichevskyi@epam.com>
2016-09-20 14:53:43 +0200

    add Header

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeCLassName="active">Home</IndexLink>
+      {" | "}
+      <IndexLink to="/about" activeCLassName="active">About</IndexLink>
+    </nav>
+  );
+}; 
+
+export default Header;
