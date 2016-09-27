d263fef63d9ec5a0662b7ee90a3f25749caf6584
Sergii Illichevskyi <Sergii_Illichevskyi@epam.com>
2016-09-27 13:04:36 +0200

    create author reducer

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import {loadCourses} from './actions/courseActions';
+import {loadAuthors} from './actions/authorActions';
 import './styles/styles.css'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
-store.dispatch(loadCourses()); 
+store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
