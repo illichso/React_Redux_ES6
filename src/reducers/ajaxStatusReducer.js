4a919bfff03a54d2c0a17665092be99c8e95bd8b
Sergii Illichevskyi <Sergii_Illichevskyi@epam.com>
2016-09-30 10:17:49 +0200

    UPDATE: create AJAX status reducer

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -8,7 +8,7 @@ function actionTypesEndsInSuccess(type) {
 export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action) {
   if (action.type == types.BEGIN_AJAX_CALL) {
     return state + 1;
-  } else if (actionTypesEndsInSuccess(action.types)) {
+  } else if (actionTypesEndsInSuccess(action.type)) {
     return state - 1;
   }
 
