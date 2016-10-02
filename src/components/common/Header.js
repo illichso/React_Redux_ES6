4d573d67214ebfb729ada02de786c503fbf36d37
Sergii Illichevskyi <Sergiy.Illichevskyy@gmail.com>
2016-10-02 13:50:39 +0200

    hide preloader based on status

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,17 +2,22 @@ import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
 import LoadingDots from './LoadingDots';
 
-const Header = () => {
+const Header = ({loading}) => {
+  debugger;
   return (
     <nav>
       <IndexLink to="/" activeCLassName="active">Home</IndexLink>
       {" | "}
-      <IndexLink to="/courses" activeCLassName="active">Courses</IndexLink>
+      <Link to="/courses" activeCLassName="active">Courses</Link>
       {" | "}
-      <IndexLink to="/about" activeCLassName="active">About</IndexLink>
-      <LoadingDots interval={100} dots={20}/>
+      <Link to="/about" activeCLassName="active">About</Link>
+      {loading && <LoadingDots interval={100} dots={20}/>}
     </nav>
   );
+};
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
