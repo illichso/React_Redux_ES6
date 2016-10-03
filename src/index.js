00d257e880f53078d60ff367d10022f88f477b27
Sergii Illichevskyi <Sergii_Illichevskyi@epam.com>
2016-10-03 09:41:47 +0200

    display save notification

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {loadCourses} from './actions/courseActions';
 import {loadAuthors} from './actions/authorActions';
 import './styles/styles.css'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
