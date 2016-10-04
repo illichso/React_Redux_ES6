9796d21b0b8d70d3159105fb5450e418a92102da
Sergii Illichevskyi <Sergii_Illichevskyi@epam.com>
2016-10-04 12:08:49 +0200

    testing the store

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -5,22 +5,47 @@ import initialState from '../reducers/initialState';
 import * as courseActions from '../actions/courseActions';
 
 describe('Store', () => {
-  it('Should handle creating courses', () => {
+  it('Should handle creating 2 courses', () => {
     //arrange
     const store = createStore(rootReducer, initialState);
-    const course = {
-      title: "Clean Code"
-    };
+    const courses = [
+      {id: 'A', title: "Clean Code"},
+      {id: 'B', title: "Refactoring"}
+    ];
 
     //act
-    const action = courseActions.createCourseSuccess(course);
+    const createFirstCourseAction = courseActions.createCourseSuccess(courses[0]);
+    store.dispatch(createFirstCourseAction);
+
+    const createSecondCourseAction = courseActions.createCourseSuccess(courses[1]);
+    store.dispatch(createSecondCourseAction);
+
+    //asset
+    const actual = store.getState().courses;
+    const expected = [
+      {id: 'A', title: "Clean Code"},
+      {id: 'B', title: "Refactoring"}
+    ];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Should handle updating 1 course', () => {
+    //arrange
+    const store = createStore(rootReducer, initialState);
+    const course = {id: 'A', title: "Clean Code"};
+
+    //act
+    const createCourseAction = courseActions.createCourseSuccess(course);
+    store.dispatch(createCourseAction);
+
+    course.title = 'New Title';
+    const action = courseActions.updateCourseSuccess(course);
     store.dispatch(action);
 
     //asset
     const actual = store.getState().courses[0];
-    const expected = {
-      title: "Clean Code"
-    };
+    const expected = {id: 'A', title: 'New Title'};
 
     expect(actual).toEqual(expected);
   });
