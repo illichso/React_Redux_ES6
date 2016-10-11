3b269845f3dcc3ca98a05610f6a95c21b2b2586c
Sergii Illichevskyi <Sergii_Illichevskyi@epam.com>
2016-10-11 12:40:15 +0200

    refactor: get rid of eslint import/export warning && update eslint rules

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
 //eslint-disable-line import/no-named-as-default
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import AuthorsPage from './components/author/AuthorsPage';
 import CoursesPage from './components/course/CoursesPage';
-import ManageAuthorPage from './components/author/ManageAuthorPage';
-import ManageCoursePage from './components/course/ManageCoursePage';
+import {ManageAuthorPage} from './components/author/ManageAuthorPage';
+import {ManageCoursePage} from './components/course/ManageCoursePage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
-    
+
     <Route path="authors" component={AuthorsPage}/>
     <Route path="author" component={ManageAuthorPage}/>
     <Route path="author/:id" component={ManageAuthorPage}/>
